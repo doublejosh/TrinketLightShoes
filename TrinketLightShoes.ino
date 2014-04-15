59aacba25825372d95a8e6cd3626f81d7e500c96
Josh Lind <jlind@tableausoftware.com>
2014-04-15 01:33:37 -0700

    initial code commmit

---
+++ TrinketLightShoes.ino
@@ -0,0 +1,111 @@
+#include <math.h>
+
+/**
+ * Sense and blink.
+ */
+
+int test_led = 1; // Trinket built in LED.
+
+// Basic setup.
+void setup() {                
+  // Initialize the digital pin as an output.
+  pinMode(test_led, OUTPUT);
+  pinMode(2, INPUT);
+}
+
+// Master looper.
+void loop() {
+  //float flexiforce_reading = 1.0;
+  int flexiforce_reading = analogRead(1);
+  //int blink_delay = map(flexiforce_reading, 0, 1023, 30, 1200);
+  float blink_delay = fscale(0, 1023, 1500, 30, flexiforce_reading, 8);
+
+  digitalWrite(test_led, HIGH);
+  delay(blink_delay);
+
+  digitalWrite(test_led, LOW);
+  delay(blink_delay);
+
+  //digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+  //delay(1000);               // wait for a second
+  //digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+  //delay(1000);               // wait for a second
+}
+
+
+/**
+ * Map with curve.
+ */
+float fscale( float originalMin, float originalMax, float newBegin, float
+newEnd, float inputValue, float curve){
+
+  float OriginalRange = 0;
+  float NewRange = 0;
+  float zeroRefCurVal = 0;
+  float normalizedCurVal = 0;
+  float rangedValue = 0;
+  boolean invFlag = 0;
+
+
+  // condition curve parameter
+  // limit range
+
+  if (curve > 10) curve = 10;
+  if (curve < -10) curve = -10;
+
+  curve = (curve * -.1) ; // - invert and scale - this seems more intuitive - postive numbers give more weight to high end on output
+  curve = pow(10, curve); // convert linear scale into lograthimic exponent for other pow function
+
+  /*
+   Serial.println(curve * 100, DEC);   // multply by 100 to preserve resolution  
+   Serial.println();
+   */
+
+  // Check for out of range inputValues
+  if (inputValue < originalMin) {
+    inputValue = originalMin;
+  }
+  if (inputValue > originalMax) {
+    inputValue = originalMax;
+  }
+
+  // Zero Refference the values
+  OriginalRange = originalMax - originalMin;
+
+  if (newEnd > newBegin){
+    NewRange = newEnd - newBegin;
+  }
+  else
+  {
+    NewRange = newBegin - newEnd;
+    invFlag = 1;
+  }
+
+  zeroRefCurVal = inputValue - originalMin;
+  normalizedCurVal  =  zeroRefCurVal / OriginalRange;   // normalize to 0 - 1 float
+
+  /*
+  Serial.print(OriginalRange, DEC);  
+   Serial.print("   ");  
+   Serial.print(NewRange, DEC);  
+   Serial.print("   ");  
+   Serial.println(zeroRefCurVal, DEC);  
+   Serial.println();  
+   */
+
+  // Check for originalMin > originalMax  - the math for all other cases i.e. negative numbers seems to work out fine
+  if (originalMin > originalMax ) {
+    return 0;
+  }
+
+  if (invFlag == 0){
+    rangedValue =  (pow(normalizedCurVal, curve) * NewRange) + newBegin;
+
+  }
+  else     // invert the ranges
+  {  
+    rangedValue =  newBegin - (pow(normalizedCurVal, curve) * NewRange);
+  }
+
+  return rangedValue;
+}
